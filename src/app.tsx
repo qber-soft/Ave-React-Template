e4764bdd9863c09a88ac1dc29d79b9f290a42ab5
rerender2021 <rerender2021@outlook.com>
2022-11-13 11:02:36 +0800

    update example

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,37 @@
 import React from "react";
-import { AveRenderer, Window, Grid } from "ave-react";
-import { Vec4 } from "ave-ui";
+import { AveRenderer, Grid, Button, Window } from "ave-react";
 
 export function App() {
   return (
-    <Window title="Ave React Template">
-      <Grid style={{ backgroundColor: new Vec4(100, 149, 237, 255) }}></Grid>
+    <Window>
+      <DemoLayout>
+        <Button text="Button"></Button>
+      </DemoLayout>
     </Window>
+  );
+}
+
+export interface IDemoLayoutProps {
+  children?: any[] | any;
+  width?: string;
+  height?: string;
+}
+
+export function DemoLayout(props: IDemoLayoutProps) {
+  const width = props?.width ?? "120dpx";
+  const height = props?.height ?? "32dpx";
+
+  const demoLayout = {
+    columns: `1 ${width} 1`,
+    rows: `1 ${height} 1`,
+    areas: {
+      center: { row: 1, column: 1 },
+    },
+  };
+  return (
+    <Grid style={{ layout: demoLayout }}>
+      <Grid style={{ area: demoLayout.areas.center }}>{props.children}</Grid>
+    </Grid>
   );
 }
 
